819f13a528c5dd6ee7005b6a145589db5c57e6b5
dandaradantas <dandaranavarro@gmail.com>
2020-04-04 15:15:48 -0700

    Adding a new component Person

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hi, I'm Dandara</h1>
+        <Person name="Dandara" age="28"/>
+        <Person name="Brenan" age="32"/>
+        <Person name="Luiz" age="22">My hobbies: Sleep</Person>
       </div>
     );
   }
