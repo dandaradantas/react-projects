c1ea80d45cfa7cfbe18c2395f973bd5cd1f6e9ea
dandaradantas <dandaranavarro@gmail.com>
2020-04-17 13:24:37 -0700

    Classes assigned dynamically

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -47,7 +47,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -70,11 +71,23 @@ class App extends Component {
           
         </div> 
       );
+
+      style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1> Hi, I'm Dandara</h1>
+        <p className={classes.join(' ')}>I'm studying React</p>
         <button 
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
