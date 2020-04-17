e3cde2012b01cf418b22d0493a08aecb04767285
dandaradantas <dandaranavarro@gmail.com>
2020-04-17 13:46:10 -0700

    Radium to set sudo selectors

--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -52,7 +53,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
     
     let persons = null;
@@ -73,6 +78,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -98,4 +107,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
