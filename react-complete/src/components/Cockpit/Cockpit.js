6b851e2d53c3ec635a0a26f2d266537573f925a1
dandaradantas <dandaranavarro@gmail.com>
2020-04-20 16:06:06 -0700

    Organizing project structure

---
+++ react-complete/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import classes from './Cockpit.css'
+
+const cockpit = (props) => {
+
+    const assignedClasses = [];
+    let btnClass = '';
+
+    if(props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if (props.persons.length <= 2) {
+      assignedClasses.push(classes.red);
+    }
+
+    if (props.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
+    }
+    
+    return (
+        <div className={classes.Cockpit}>
+            <h1> Hi, I'm Dandara</h1>
+            <p className={assignedClasses.join(' ')}>I'm studying React</p>
+            {/* This name 'myAlt' is up to you */}
+            <button className={btnClass}
+            onClick={props.clicked}>
+                Toggle Persons
+            </button>
+        </div>
+    );
+};
+
+export default cockpit;
--- react-complete/src/containers/App.js
+++ react-complete/src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 //the name 'classes' is up to you
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -48,52 +49,25 @@ class App extends Component {
 
   render() {
      let persons = null;
-     let btnClass = '';
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person 
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
-          
-        </div> 
-      );
-
-     btnClass = classes.Red;
-    }
-
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
+      persons = <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}/> ;
     }
 
     return (
         <div className={classes.App}>
-          <h1> Hi, I'm Dandara</h1>
-          <p className={assignedClasses.join(' ')}>I'm studying React</p>
-          {/* This name 'myAlt' is up to you */}
-          <button className={btnClass}
-            onClick={this.togglePersonsHandler}>
-              Toggle Persons
-          </button>
-    
+            <Cockpit 
+              showPersons={this.state.showPersons}
+              persons={this.state.persons} 
+              clicked={this.togglePersonsHandler}
+            />
             {persons}    
         </div>
     );
   }
 }
-
-// Just when using Radium
-//export default Radium(App);
 
 export default App;
