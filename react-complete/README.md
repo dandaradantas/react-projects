ef26738e0c28bc3002f2dddfadad0e97fa6eaa2b
dandaradantas <dandaranavarro@gmail.com>
2020-04-20 09:57:38 -0700

    CSS Modules configuration and usage

--- react-complete/README.md
+++ react-complete/README.md
@@ -14,8 +14,37 @@ And finally, go to the end of your component file, where your export it, and you
 
 ``` export default Radium(App); ```
 
-### Styled Components library
+### Styled Components library (not a good approach)
 
 ``` npm install --save styled-components ```
 
 Remember that using this package we are going to use regular CSS, this is, instead of *backgroundColor: 'green'* we have to use *background-color: green*, for example.
+
+### Command to access configurations that are under the hood in the project
+
+```npm run eject```
+
+Running this we will have access to the folder **config**
+
+We did some configurations in the files *webpack.config.dev.js* and *webpack.config.prod.js*. See below that we add *modules* and *localIdentName* to the *options* in the test css part 
+
+```sh
+test: /\.css$/,
+use: [
+    require.resolve('style-loader'),
+    {
+    loader: require.resolve('css-loader'),
+    options: {
+        importLoaders: 1,
+        modules: true, 
+        localIdentName: '[name]__[local]__[hash:base64:5]'
+    },
+    },...
+
+```
+
+This configuration is for use *CSS Module* which, in my opinion, is better than the *Radium* and than *styled-components* because the JS and CSS files will be separated.
+
+More about [how to use CSS Modules with create-react-app](https://medium.com/nulogy/how-to-use-css-modules-with-create-react-app-9e44bec2b5c2)
+
+[CSS Modules GitHub]( https://github.com/css-modules/css-modules)
--- react-complete/src/App.js
+++ react-complete/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+//the name 'classes' is up to you
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -46,20 +47,8 @@ class App extends Component {
   }
 
   render() {
-    //const style = {
-      // backgroundColor: 'green',
-      // color: 'white',
-      // font: 'inherit',
-      // border: '1px solid blue',
-      // padding: '8px',
-      // cursor: 'pointer',
-      // ':hover': {
-      //   backgroundColor: 'lightgreen',
-      //   color: 'black'
-      // }
-   // }
-
-   let persons = null;
+     let persons = null;
+     let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -76,28 +65,24 @@ class App extends Component {
         </div> 
       );
 
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
+     btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1> Hi, I'm Dandara</h1>
-          <p className={classes.join(' ')}>I'm studying React</p>
+          <p className={assignedClasses.join(' ')}>I'm studying React</p>
           {/* This name 'myAlt' is up to you */}
-          <button className="button"
+          <button className={btnClass}
             onClick={this.togglePersonsHandler}>
               Toggle Persons
           </button>
