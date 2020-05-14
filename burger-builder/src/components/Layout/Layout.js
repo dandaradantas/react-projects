662141d0e3d90a70c0c007025427e13ab68ede3a
dandaradantas <dandaranavarro@gmail.com>
2020-05-13 21:48:05 -0700

    Adding navigation bar and Side drawer

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
 
 const layout = ( props ) => (
     <Auxiliary>
+        <Toolbar />
+        <SideDrawer />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className={classes.Content}>
             {props.children}
