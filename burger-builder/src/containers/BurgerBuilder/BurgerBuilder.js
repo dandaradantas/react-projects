89751076d1ed56484ed23c39c8ce3663937b4bc8
dandaradantas <dandaranavarro@gmail.com>
2020-07-09 00:12:28 -0700

    Adding style to the Checkout Summary and developing a simple form to Contact info

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,32 +87,13 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: "Dandara Navarro",
-        //         address: {
-        //             street: "Teststree 6",
-        //             zipCode: "V55698",
-        //             country: "Germany"
-        //         },
-        //         email: "test@test.com"
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false})
-        //     })  
-        //     .catch(error => this.setState({loading: false, purchasing: false}))
-        const queryParams = [];
+                const queryParams = [];
         for (let i in this.state.ingredients) {
             let key = encodeURIComponent(i);
             let value = encodeURIComponent(this.state.ingredients[i]);
             queryParams.push(key + '=' + value);
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
         this.props.history.push({
             pathname: '/checkout',
